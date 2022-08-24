29f50ef730299d2cd657c35ce045de8cc91354e7
Ronaldo Rios Espíndola <ronaldoriosespindola@gmail.com>
2022-08-24 01:48:38 -0300

    final commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Inicial Data:
+// INICIAL DATA:
 
 let square = {
-    a1:'', a2:'', a3:'',
-    b1:'', b2:'', b3:'',
-    c1:'', c2:'', c3:''
+    a1:'',a2:'',a3:'',
+    b1:'',b2:'',b3:'',
+    c1:'',c2:'',c3:''
 }
 
 let playerTurn = '';
@@ -13,31 +13,31 @@ let warning = ''
 let playing = false;
 
 
+reset();
 
 
-
-//Events:
+//EVENTS:
 
 document.querySelector('.reset').addEventListener('click', reset)
-document.querySelectorAll('.item').forEach(item => { item.addEventListener('click', itemClick ) })
+document.querySelectorAll('.item').forEach(item => { item.addEventListener('click', itemClick ) });
 
 
 
 
 
 
-//Functions:
+//FUNCTIONS:
 
 function itemClick(event){
     let item = event.target.getAttribute('data-item')
 
-    if(square[item] === ''){
+    if(playing && square[item] === ''){
         square[item] = playerTurn 
-    }
+        renderSquare() 
+        togglePlayer()
+    };
     
-    renderSquare()
-
-    togglePlayer()
+    
 }
 
 //change player:
@@ -48,14 +48,17 @@ function togglePlayer() {
     else {
         playerTurn = 'x'
     }
+    renderInfo()
 }
+
+
 function reset(){
     //clean warnings:
     warning = ''
 
     // whats turn of the player:
     let random = Math.floor(Math.random() * 2 )
-    if(random == 0){
+    if(random === 0){
         playerTurn = 'x'
     }
     else {
@@ -67,27 +70,79 @@ function reset(){
         square[e] = ''
     }
 
-    // reset and start game:
-    playing = true
+    renderSquare();
+    renderInfo();
 
-    renderSquare()
-    renderInfo()
+     // reset and start game:
+     playing = true;
 }
 
 function renderSquare() {
     for( let e in square ){
-        let item = document.querySelector(`div[data-item=${e}]`)
+        let item = document.querySelector(`div[data-item=${e}]`);
+        item.innerHTML = square[e]
+        
+       
+    };
 
-        if(square[e] !== ''){
-            item.innerHTML = square[e]
-        }
-        else {
-            item.innerHTML = ''
-        }
-    }
+    checkGame();
 }
 
 function renderInfo(){
     document.querySelector('.vez').innerHTML = playerTurn
-    document.querySelector('resultado').innerHTML = warning
+    document.querySelector('.resultado').innerHTML = warning
+}
+
+// Verify the options case exists winner:
+function checkGame(){
+    if(checkWinnerFor('x')){
+        warning = 'O "x" venceu!';
+        playing = false;
+    }
+    else if(checkWinnerFor('o')){
+        warning = 'O "o" venceu!';
+        playing = false;
+    }
+    else if(isFull()){
+        warning = 'Deu empate';
+        playing = false;
+    }
+
+};
+
+function checkWinnerFor(player){
+    let possibilities = [
+    'a1,a2,a3', 
+    'b1,b2,b3', 
+    'c1,c2,c3',
+
+    'a1,b1,c1',
+    'a2,b2,b3',
+    'a3,b3,c3',
+
+    'a1,b2,c3',
+    'a3,b2,c1',
+    ];
+
+    for(let k in possibilities){
+        let pArray = possibilities[k].split(',')
+        let hasWon = pArray.every(option => square[option] === player);
+
+        if(hasWon){
+            return true;
+        }
+    }
+
+    return false;
+
+};
+
+function isFull(){
+    for( let i in square){
+        if(square[i] === ''){
+            return false;
+        }
+    }
+    return true;
+
 }
